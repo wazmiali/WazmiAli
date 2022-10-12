26165609cbf7eadd52c8afcd756ec74e6253cc90
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2022-10-12 11:17:39 +0530

    wazmi ali

---
+++ program.txt.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class student
+{
+	public:
+	char s_name[10];
+	int rollno;
+	public:
+	void enterdata()
+	{
+		cout<<"enter student name:";
+		for(int i=0;i<10;i++)
+		{
+			cin>>s_name[i];
+		}
+		cout<<"enter roll no:";
+		cin>>rollno;
+	}
+	void showdata();
+};
+void student::showdata()
+{
+	cout<<"list of student:";
+	for(int i=0;i<10;i++)
+	{
+		cout<<s_name[i]<<"   ";
+	}
+	cout<<rollno;
+}
+int main()
+{
+	student s;
+	s.enterdata();
+	s.showdata();
+	return 0;
+	
+}
